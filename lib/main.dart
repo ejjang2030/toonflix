99146e6e11dd0e40eeed556c72db7bd2b3dbeb74
ejjang2030 <ejjang2030@gmail.com>
2023-02-05 19:22:44 +0900

    Widget Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,17 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   List<int> numbers = [];
 
+  bool showTitle = true;
+
   void onClicked() {
     numbers.add(numbers.length);
     print(numbers);
+  }
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
   }
 
   @override
@@ -32,7 +40,11 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [const MyLargeTitle()],
+            children: [
+              showTitle ? const MyLargeTitle() : Text('nothing'),
+              IconButton(
+                  onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye))
+            ],
           ),
         ),
       ),
@@ -40,13 +52,33 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState!');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build!');
     return Text(
       'My Large Title',
       style: TextStyle(
