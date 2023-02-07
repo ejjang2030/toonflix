e2fbd61e25a23a03db17a81bb3ed4d847bff58f9
ejjang2030 <ejjang2030@gmail.com>
2023-02-07 23:19:44 +0900

    ListView.builder and separated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:toonflix/services/api_service.dart';
 
 import '../models/webtoon_model.dart';
@@ -6,7 +7,7 @@ import '../models/webtoon_model.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +16,21 @@ class HomeScreen extends StatelessWidget {
           future: webtoons,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Text("There is data!");
-            } else {
-              return Text('Loading...');
+              var list = snapshot.data! as List<WebtoonModel>;
+              return ListView.separated(
+                separatorBuilder: (context, index) => SizedBox(width: 20),
+                scrollDirection: Axis.horizontal,
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  print(index);
+                  var webtoon = list[index];
+                  return Text(webtoon.title);
+                },
+              );
             }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
           }),
       backgroundColor: Colors.white,
       appBar: AppBar(
